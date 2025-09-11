229882039209df1c050ad73346b4a41388b90293
Angerbode <jdr@frichs-pyrolysis.com>
2025-09-12 00:25:44 +0200

    text node exersice

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode(text='some anchor',text_type=TextType.LINK, url='http://localhost')
+    print(node)
+
+if __name__ == "__main__":
+    main()
+
