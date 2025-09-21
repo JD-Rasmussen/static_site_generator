1eb4ab544ef1a5e2e8d17976e5c8f9e5d2ac0298
Angerbode <jdr@frichs-pyrolysis.com>
2025-09-21 23:56:25 +0200

    eod

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,3 +22,18 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+
+
+class LeafNode(HTMLNode):
+
+    def __init__(self, value, tag = None, props = None):
+        if value is None:
+            raise ValueError("LeafNode error value missing")
+        super().__init__(tag = tag, value = value, children = None, props = props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("LeafNode error value missing")
+        if self.tag == None:
+            return self.value
+        else return '<{self.tag}[{self.props}]>{self.value}</{self.tag}> '
