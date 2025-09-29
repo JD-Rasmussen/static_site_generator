e0fdecaad3bf6c096f7ec11d869f7e0974711a92
Angerbode <jdr@frichs-pyrolysis.com>
2025-09-29 22:54:50 +0200

    TextNode to HTMLNode

--- src/test_textnode_to_htmlnode.py
+++ src/test_textnode_to_htmlnode.py
@@ -9,38 +9,38 @@ class Test_text_node_to_html_node(unittest.TestCase):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
-        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.to_html(), "This is a text node")
 
     def test_bold(self):
         node = TextNode("This is a bold text node", TextType.BOLD)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
-        self.assertEqual(html_node.value, "<b>This is a bold text node</b>")
+        self.assertEqual(html_node.to_html(), "<b>This is a bold text node</b>")
 
     def test_italic(self):
         node = TextNode("This is an italic text node", TextType.ITALIC)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "i")
-        self.assertEqual(html_node.value, "<i>This is an italic text node</i>")
+        self.assertEqual(html_node.to_html(), "<i>This is an italic text node</i>")
 
     def test_code(self):
         node = TextNode("This is a code text node", TextType.CODE)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "code")
-        self.assertEqual(html_node.value, "<code>This is a code text node</code>")
+        self.assertEqual(html_node.to_html(), "<code>This is a code text node</code>")
 
     def test_link(self):
         node = TextNode("google", TextType.LINK, url = "www.google.com")
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "a")
-        self.assertEqual(html_node.value, '<a href="https://www.google.com">google</a>')
+        self.assertEqual(html_node.to_html(), '<a href="www.google.com">google</a>')
 
 
     def test_img(self):
         node = TextNode("This is an img text node", TextType.IMAGE, url="google.com")
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
-        self.assertEqual(html_node.value, '<img src="google.com" alt="This is an img text node">')
+        self.assertEqual(html_node.to_html(), '<img src="google.com" alt="This is an img text node"></img>')
 
 
 
