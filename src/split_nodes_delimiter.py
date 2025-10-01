8b02ad7b1a9977f0468c7ae12d840c95a7cbe778
Angerbode <jdr@frichs-pyrolysis.com>
2025-10-01 22:25:36 +0200

    eod

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -1,8 +1,20 @@
 
 
+from src.htmlnode import HTMLNode, LeafNode, ParentNode
+from src.textnode import TextNode, TextType
+from src.textnode_to_htmlnode import text_node_to_html_node
 
+[
+    TextNode("This is text with a ", TextType.TEXT),
+    TextNode("bolded phrase", TextType.BOLD),
+    TextNode(" in the middle", TextType.TEXT),
+]
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    
+        
+
+
+
+
 
