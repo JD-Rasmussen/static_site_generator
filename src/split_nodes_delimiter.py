a1acb900129465bf7c2739e06a730f5c61442173
Angerbode <jdr@frichs-pyrolysis.com>
2025-10-07 01:05:49 +0200

    split delimiter complete

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -14,21 +14,36 @@ from src.textnode_to_htmlnode import text_node_to_html_node
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     result = []    
     for node in old_nodes:
-        if node.text_type == text_type.TEXT:
-            before = TextNode("", text_type.TEXT, node.url)
-            middle = TextNode("", text_type)
-            after = TextNode("", text_type.TEXT, node.url)
-            if delimiter in node.text:
-                before.text, secondtext = node.text.split(delimiter, 1)
-                result.append(before)
-                    if delimiter not in secondtext:
-                        raise Exception ("missing end delimiter")
-                middle.text, after.text = secondtext.split(delimiter,1)   
-                result.append(middle)
-                result.append(after)
-            else:
-                result.append(node)
+        if node.text_type == TextType.TEXT:
+            parts = node.text.split(delimiter)
+            if len(parts)% 2 == 0: 
+                raise Exception("missing end delimiter")
+            for i, part in enumerate(parts):
+                if part == "": continue
+                chosen_type = text_type if i % 2 == 1 else TextType.TEXT
+                result.append(TextNode(part, chosen_type))
+        else:
+            result.append(node)
+    return result
 
+
+
+
+#       if node.text_type == text_type.TEXT:
+#            before = TextNode("", text_type.TEXT, node.url)
+#            middle = TextNode("", text_type)
+#            after = TextNode("", text_type.TEXT, node.url)
+#            if delimiter in node.text:
+#                before.text, secondtext = node.text.split(delimiter, 1)
+#                result.append(before)
+#                    if delimiter not in secondtext:
+#                        raise Exception ("missing end delimiter")
+#                middle.text, after.text = secondtext.split(delimiter,1)   
+#                result.append(middle)
+#                result.append(after)
+#             else:
+#                result.append(node)
+#
 
 
 
---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,48 @@
+import unittest
+
+from src.textnode import TextNode, TextType
+from src.split_nodes_delimiter import split_nodes_delimiter
+
+class TestSplit_node_delimiter(unittest.TestCase):
+
+
+    def test_No_Delimiter(self):
+            node = TextNode("This is a text node", TextType.TEXT)
+            split_node = split_nodes_delimiter([node], "`", TextType.CODE)
+            self.assertEqual(split_node[0], node)
+            self.assertEqual(len(split_node),1)
+            self.assertEqual(split_node[0].text, "This is a text node")
+            self.assertEqual(split_node[0].text_type, TextType.TEXT)
+    
+    def test_one_Delimiter(self):
+        node = TextNode("This is a **bold** text node", TextType.TEXT)
+        split_node = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(split_node),3)
+        self.assertEqual(split_node[0].text, "This is a ")
+        self.assertEqual(split_node[0].text_type, TextType.TEXT)
+        self.assertEqual(split_node[1].text, "bold")
+        self.assertEqual(split_node[1].text_type, TextType.BOLD)
+        self.assertEqual(split_node[2].text, " text node")
+        self.assertEqual(split_node[2].text_type, TextType.TEXT)
+
+    def test_two_Delimiter(self):
+        node = TextNode("This is an _italic_ text _node_ multi split", TextType.TEXT)
+        split_node = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(len(split_node),5)
+        self.assertEqual(split_node[0].text, "This is an ")
+        self.assertEqual(split_node[0].text_type, TextType.TEXT)
+        self.assertEqual(split_node[1].text, "italic")
+        self.assertEqual(split_node[1].text_type, TextType.ITALIC)
+        self.assertEqual(split_node[2].text, " text ")
+        self.assertEqual(split_node[2].text_type, TextType.TEXT)
+        self.assertEqual(split_node[3].text, "node")
+        self.assertEqual(split_node[3].text_type, TextType.ITALIC)
+        self.assertEqual(split_node[4].text, " multi split")
+        self.assertEqual(split_node[4].text_type, TextType.TEXT)        
+
+
+
+
+
+
+
