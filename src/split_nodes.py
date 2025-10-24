600138e4024363d2e4d38a61254c6d42b3ded1ad
Angerbode <jdr@frichs-pyrolysis.com>
2025-10-24 23:51:12 +0200

    eod

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -26,8 +26,15 @@ def split_nodes_link(old_nodes):
     result = []
     for node in old_nodes:
         if node.text_type == TextType.TEXT:
-        parts = node.text.split(extract_markdown_links(text))
+            extracted_parts = extract_markdown_links(node.text)
+            for link_tuple in extracted_parts:
+                parts[i] = link_tuple[].split(link_tuple[0])
         if len(parts)% 2 == 0: 
+            raise Exception("missing end delimiter")
+        for i, part in enumerate(parts):
+            if part == "": continue
+            chosen_type = text_type if i % 2 == 1 else TextType.TEXT
+            result.append(TextNode(part, chosen_type))
         else:
             result.append(node)
     return result
