e1b8aab4783b2bea756589e0c1904e04e33db59e
Angerbode <jdr@frichs-pyrolysis.com>
2025-11-01 23:47:24 +0100

    eod

--- src/extract_markdowns.py
+++ src/extract_markdowns.py
@@ -11,3 +11,19 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     result = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return result
+
+
+def markdown_to_blocks(markdown):
+
+    result = [part.strip() for part in markdown.split("\n\n")]
+
+    return result
+
+
+
+
+
+
+
+
+
