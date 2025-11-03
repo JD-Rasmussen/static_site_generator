5b69897d50e94585c703fe46bb88052574c0ccd5
Angerbode <jdr@frichs-pyrolysis.com>
2025-11-03 23:52:18 +0100

    eod

--- src/test_extract_markdowns.py
+++ src/test_extract_markdowns.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.extract_markdowns import extract_markdown_images, extract_markdown_links
+from src.extract_markdowns import extract_markdown_images, extract_markdown_links, markdown_to_blocks
 
 class TestSplit_node_delimiter(unittest.TestCase):
 
@@ -21,3 +21,23 @@ class TestSplit_node_delimiter(unittest.TestCase):
         text = "![pic](img.png) and [site](https://x.y)"
         self.assertListEqual([("pic", "img.png")], extract_markdown_images(text))
         self.assertListEqual([("site", "https://x.y")], extract_markdown_links(text))
+    
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
