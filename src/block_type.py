9dc2fea7445c5cacdaa4b9365f5054ef64fb86ed
Angerbode <jdr@frichs-pyrolysis.com>
2025-11-05 00:07:54 +0100

    eodddd

--- src/block_type.py
+++ src/block_type.py
@@ -1,6 +1,6 @@
 from enum import Enum
-
-
+import re
+from extract_markdowns import markdown_to_blocks
 
 class BlockType(Enum):
     PARAGRAPH       = "paragraph"
@@ -13,13 +13,28 @@ class BlockType(Enum):
 
 
 def block_to_block_type(markdown):
+    lines = markdown.split("\n")
+    if not lines:
+        return BlockType.PARAGRAPH
 
-    heading = markdow.split("#", maxsplit=1)
+    if lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
 
+    elif re.match(r'#{1,6}\s', lines[0]):
+        return BlockType.HEADING
 
+    elif all(i.startswith(">") for i in lines):
+        return BlockType.QUOTE
+    
+    elif all(i.startswith("- ") for i in lines):
+        return BlockType.UNORDERED_LIST
+    
+    elif all(line.startswith(f"{i}. ") for i, line in enumerate(lines, start=1)):
+        return BlockType.ORDERED_LIST
 
+    else:
+        return BlockType.PARAGRAPH
 
-    return block_type
 
 
 
