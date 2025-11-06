8241d9710b400e73348dbc52adfabbf4a2e1c814
Angerbode <jdr@frichs-pyrolysis.com>
2025-11-06 22:45:31 +0100

    block types

---
+++ src/test_block_type.py
@@ -0,0 +1,47 @@
+import unittest
+
+from src.block_type import BlockType, block_to_block_type
+
+class Testblock_to_block_type(unittest.TestCase):
+    
+    def test_block_type_heading(self):
+        md = "# Title"
+        assert block_to_block_type(md) == BlockType.HEADING
+    
+    def test_block_type_code(self):
+        md = "```py\nx = 1\n```"
+        assert block_to_block_type(md) == BlockType.CODE
+
+    def test_block_type_quote(self):
+        md = "> a\n> b"
+        assert block_to_block_type(md) == BlockType.QUOTE
+   
+    def test_block_type_unordered_list(self):
+        md = "- a\n- b"
+        assert block_to_block_type(md) == BlockType.UNORDERED_LIST
+    # python
+    def test_block_type_ordered_list(self):
+        md = "1. a\n2. b\n3. c"
+        assert block_to_block_type(md) == BlockType.ORDERED_LIST
+
+    def test_block_type_paragraph(self):
+        md = "just some text"
+        assert block_to_block_type(md) == BlockType.PARAGRAPH
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
